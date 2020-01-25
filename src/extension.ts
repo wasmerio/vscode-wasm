757f63e0a807224f386fc8b0ebe611e6ed87411c
Syrus <me@syrusakbary.com>
2020-01-24 17:00:41 -0800

    Prettified code

--- src/extension.ts
+++ src/extension.ts
@@ -1,57 +1,73 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode'
+import * as vscode from "vscode";
 
-import WebAssemblyContentProvider from './webassembly-content-provider'
-import { Uri } from 'vscode'
-import { wasm2wat, wat2wasm, writeFile, readFile } from './utils'
+import WebAssemblyContentProvider from "./webassembly-content-provider";
+import { Uri } from "vscode";
+import { wasm2wat, wat2wasm, writeFile, readFile } from "./utils";
 
 export function activate(context: vscode.ExtensionContext) {
-  const provider = new WebAssemblyContentProvider()
+  const provider = new WebAssemblyContentProvider();
 
-  const registration = vscode.workspace.registerTextDocumentContentProvider('wasm-preview', provider)
+  const registration = vscode.workspace.registerTextDocumentContentProvider(
+    "wasm-preview",
+    provider
+  );
 
-  const openEvent = vscode.workspace.onDidOpenTextDocument((document: vscode.TextDocument) => {
-    showDocument(document);
-  });
-
-  const previewCommand = vscode.commands.registerCommand('wasm.wasm2wat', (uri: Uri) => {
-    showPreview(uri)
-  })
-
-  const save2watCommand = vscode.commands.registerCommand('wasm.save2wat', (uri: Uri) => {
-    const watPath = uri.path.replace(/\.wasm$/, '.wat')
-
-    const saveDialogOptions = {
-      filters: {
-        'WebAssembly Text': ['wat', 'wast'],
-        'WebAssembly Binary': ['wasm']
-      },
-      defaultUri: uri.with({ scheme: 'file', path: watPath })
+  const openEvent = vscode.workspace.onDidOpenTextDocument(
+    (document: vscode.TextDocument) => {
+      showDocument(document);
     }
+  );
 
-    const from = uri.with({ scheme: 'file' })
-
-    vscode.window.showSaveDialog(saveDialogOptions)
-      .then(maybeSaveWat(from), vscode.window.showErrorMessage)
-  })
-
-  const save2wasmCommand = vscode.commands.registerCommand('wasm.save2wasm', (uri: Uri) => {
-    const wasmPath = uri.path.replace(/\.wat$/, '.wasm')
-
-    const saveDialogOptions = {
-      filters: {
-        'WebAssembly Binary': ['wasm'],
-        'WebAssembly Text': ['wat', 'wast']
-      },
-      defaultUri: uri.with({ scheme: 'file', path: wasmPath })
+  const previewCommand = vscode.commands.registerCommand(
+    "wasm.wasm2wat",
+    (uri: Uri) => {
+      showPreview(uri);
     }
+  );
 
-    const from = uri.with({ scheme: 'file' })
+  const save2watCommand = vscode.commands.registerCommand(
+    "wasm.save2wat",
+    (uri: Uri) => {
+      const watPath = uri.path.replace(/\.wasm$/, ".wat");
 
-    vscode.window.showSaveDialog(saveDialogOptions)
-      .then(maybeSaveWasm(from), vscode.window.showErrorMessage)
-  })
+      const saveDialogOptions = {
+        filters: {
+          "WebAssembly Text": ["wat", "wast"],
+          "WebAssembly Binary": ["wasm"]
+        },
+        defaultUri: uri.with({ scheme: "file", path: watPath })
+      };
+
+      const from = uri.with({ scheme: "file" });
+
+      vscode.window
+        .showSaveDialog(saveDialogOptions)
+        .then(maybeSaveWat(from), vscode.window.showErrorMessage);
+    }
+  );
+
+  const save2wasmCommand = vscode.commands.registerCommand(
+    "wasm.save2wasm",
+    (uri: Uri) => {
+      const wasmPath = uri.path.replace(/\.wat$/, ".wasm");
+
+      const saveDialogOptions = {
+        filters: {
+          "WebAssembly Binary": ["wasm"],
+          "WebAssembly Text": ["wat", "wast"]
+        },
+        defaultUri: uri.with({ scheme: "file", path: wasmPath })
+      };
+
+      const from = uri.with({ scheme: "file" });
+
+      vscode.window
+        .showSaveDialog(saveDialogOptions)
+        .then(maybeSaveWasm(from), vscode.window.showErrorMessage);
+    }
+  );
 
   if (vscode.window.activeTextEditor) {
     showDocument(vscode.window.activeTextEditor.document);
@@ -62,60 +78,65 @@ export function activate(context: vscode.ExtensionContext) {
     openEvent,
     previewCommand,
     save2watCommand,
-    save2wasmCommand,
-  )
+    save2wasmCommand
+  );
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
 
 function showDocument(document: vscode.TextDocument): void {
-  if (document.languageId === "wasm" && document.uri.scheme !== "wasm-preview") {
-    vscode.commands.executeCommand("workbench.action.closeActiveEditor").then(() => {
-      showPreview(document.uri);
-    }, vscode.window.showErrorMessage);
+  if (
+    document.languageId === "wasm" &&
+    document.uri.scheme !== "wasm-preview"
+  ) {
+    vscode.commands
+      .executeCommand("workbench.action.closeActiveEditor")
+      .then(() => {
+        showPreview(document.uri);
+      }, vscode.window.showErrorMessage);
   }
 }
 
 function showPreview(uri: vscode.Uri): void {
   if (uri.scheme === "wasm-preview") {
-    return
+    return;
   }
 
-  vscode.commands.executeCommand('vscode.open', uri.with({ scheme: 'wasm-preview' }))
+  vscode.commands
+    .executeCommand("vscode.open", uri.with({ scheme: "wasm-preview" }))
     .then(null, vscode.window.showErrorMessage);
 }
 
 function maybeSaveWat(from: vscode.Uri) {
   return (to: vscode.Uri | undefined) => {
     if (!to) {
-      return
+      return;
     }
 
-    return saveWat(from, to)
-  }
+    return saveWat(from, to);
+  };
 }
 
 async function saveWat(from: vscode.Uri, to: vscode.Uri) {
-  const wasmContent = await readFile(from)
-  const watContent = wasm2wat(wasmContent)
+  const wasmContent = await readFile(from);
+  const watContent = wasm2wat(wasmContent);
 
-  await writeFile(to, watContent)
+  await writeFile(to, watContent);
 }
 
 function maybeSaveWasm(from: vscode.Uri) {
   return (to: vscode.Uri | undefined) => {
     if (!to) {
-      return
+      return;
     }
 
-    return saveWasm(from, to)
-  }
+    return saveWasm(from, to);
+  };
 }
 
 async function saveWasm(from: vscode.Uri, to: vscode.Uri) {
-  const watContent = await readFile(from)
-  const wasmContent = wat2wasm(watContent)
+  const watContent = await readFile(from);
+  const wasmContent = wat2wasm(watContent);
 
-  await writeFile(to, wasmContent)
+  await writeFile(to, wasmContent);
 }
--- src/webassembly-content-provider.ts
+++ src/webassembly-content-provider.ts
@@ -1,19 +1,22 @@
-'use strict';
+"use strict";
 
-import { TextDocumentContentProvider, Uri, window } from 'vscode'
-import { wasm2wat, readWasm } from './utils'
+import { TextDocumentContentProvider, Uri, window } from "vscode";
+import { wasm2wat, readWasm } from "./utils";
 
 /**
  * This class helps to open WebAssembly binary files.
  */
-export default class WebAssemblyContentProvider implements TextDocumentContentProvider {
-  public async provideTextDocumentContent(uri: Uri): Promise<string | undefined> {
-    const buffer = await readWasm(uri)
+export default class WebAssemblyContentProvider
+  implements TextDocumentContentProvider {
+  public async provideTextDocumentContent(
+    uri: Uri
+  ): Promise<string | undefined> {
+    const buffer = await readWasm(uri);
 
     if (!buffer) {
-      return
+      return;
     }
 
-    return wasm2wat(buffer)
+    return wasm2wat(buffer);
   }
 }
